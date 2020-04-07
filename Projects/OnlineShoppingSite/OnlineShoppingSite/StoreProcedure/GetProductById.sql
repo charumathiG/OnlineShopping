52e5f6f4b44d0a829756c0ede7f97bbaf1503f8e
charumathiG <57354228+charumathiG@users.noreply.github.com>
2020-04-07 16:43:19 +0530

    updates

---
+++ Projects/OnlineShoppingSite/OnlineShoppingSite/StoreProcedure/GetProductById.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [dbo].[GetProductById]
+(
+@ProductId VARCHAR(5)
+)
+AS
+BEGIN
+SELECT
+a.ProductId,
+a.CustomerId,
+c.FullName,
+p.ProductName,
+a.Price,
+a.Quantity,
+a.Date
+
+FROM AddToCart a INNER JOIN Products p 
+ON
+a.ProductId = p.ProductId
+
+INNER JOIN Customers c 
+ON 
+c.CustomerId = a.CustomerId
+
+WHERE 
+a.ProductId = @ProductId
+
+END
+
+exec test 'P0002'
---
+++ Projects/OnlineShoppingSite/OnlineShoppingSite/StoreProcedure/TotalPrice.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[Cart]
+	(
+	@ProductId varchar(5),
+	@CustomerId varchar(100),
+	@Price numeric(7,2),
+	@Image varchar(50),
+	@Quantity int
+	)
+as
+begin
+select @Price = (select Price from Products where ProductId=@ProductId) 
+update OrderDetail set Price=@Price*@Quantity where ProductId=@ProductId
+end
+
+
