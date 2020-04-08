7f0112b38f0b9dbfbe20eedad9cc9bccb244a348
charumathiG <57354228+charumathiG@users.noreply.github.com>
2020-04-08 19:28:24 +0530

    updates

---
+++ Projects/OnlineShoppingSite/EcommerceModels/AuthenticateModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EcommerceModels
+{
+
+    /// <summary>
+    /// Implementation.
+    /// </summary>
+    public class AuthenticateModel
+    {
+        /// <summary>
+        /// Gets or sets implementation of a property.
+        /// </summary>
+        [Required]
+        public string EmailId { get; set; }
+
+        /// <summary>
+        /// Gets or sets implementation of a property.
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+    }
+}
+
---
+++ Projects/OnlineShoppingSite/EcommerceModels/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EcommerceModels
+{
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets implementation of property.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets implementation of property.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets implementation of property.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets implementation of property.
+        /// </summary>
+        public string EmailId { get; set; }
+
+        /// <summary>
+        /// Gets or sets implementation of property.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets implementation of property.
+        /// </summary>
+        public string Token { get; set; }
+    }
+}
+
